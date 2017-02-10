23a35f93315ec0b537dc3b35eb279aaee538173c
Alexander Nagel <t00189485@365s.ittralee.ie>
2017-02-10 12:47:32 +0000

    Initial Commit
    Added .gitignore
    Added 01_bash
    Added whereami.sh
    Added worbook1.ipynb

---
+++ whereami.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "You are in /home/t00189485"
+echo "total 32
+-rwxr-xr-x  1 Administrators Domain Users 12610 Feb 10 11:43 01_bash_setting_permissions.ipynb
+drwxr-xr-x+ 1 t00189485      Domain Users     0 Feb 10 11:39 s8
+-rwxr--r--  1 t00189485      Domain Users  1965 Feb 10 12:08 whereami.sh
+-rwxr-xr-x  1 t00189485      Domain Users 10658 Feb 10 12:06 workbook1.ipynb"
+echo "Filesystem         1K-blocks       Used     Available Use% Mounted on
+C:/cygwin64        934570312  494966288     439604024  53% /
+K:                1073753084  480890652     592862432  45% /cygdrive/k
+M:                2959300616 1875025124    1084275492  64% /cygdrive/m
+N:                 100584316   86867732      13716584  87% /cygdrive/n
+X:             1072668082176          0 1072668082176   0% /cygdrive/x"
+echo "total 60
+drwxr-xr-x+ 1 t00189485                     Domain Users             0 Feb 10 12:06 .
+drwxrwxrwt+ 1 1XX64006A3DA878+Administrator 1XX64006A3DA878+None     0 Feb 10 11:17 ..
+-rwxr-xr-x  1 t00189485                     Domain Users          1494 Oct  1 14:58 .bash_profile
+-rwxr-xr-x  1 t00189485                     Domain Users          6054 Oct  1 14:58 .bashrc
+-rwxr-xr-x  1 t00189485                     Domain Users          1919 Oct  1 14:58 .inputrc
+drwxr-xr-x+ 1 t00189485                     Domain Users             0 Feb 10 11:43 .ipynb_checkpoints
+drwxr-xr-x+ 1 t00189485                     Domain Users             0 Feb 10 11:31 .ipython
+-rwxr-xr-x  1 t00189485                     Domain Users          1236 Oct  1 14:58 .profile
+-rwxr-xr-x  1 Administrators                Domain Users         12610 Feb 10 11:43 01_bash_setting_permissions.ipynb
+drwxr-xr-x+ 1 t00189485                     Domain Users             0 Feb 10 11:39 s8
+-rwxr--r--  1 t00189485                     Domain Users          1965 Feb 10 12:08 whereami.sh
+-rwxr-xr-x  1 t00189485                     Domain Users         10658 Feb 10 12:06 workbook1.ipynb"
+
